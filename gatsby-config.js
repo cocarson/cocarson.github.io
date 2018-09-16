607bf1416913a35c3a5319da9a605c070c3cdcda
Carson Covell <cocarson@umich.edu>
2018-09-16 09:22:04 -0400

    Updates

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Carson Covell',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
---
+++ path----8d5e8aafc91a1291c953.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Carson Covell"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----8d5e8aafc91a1291c953.js.map
